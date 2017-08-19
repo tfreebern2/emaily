894f987aadf58ce275d7da782ed896ac9d29cfa2
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-19 15:23:00 -0400

    Credits display in Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,10 @@ class Header extends Component {
 					<li key="1">
 						<Payments />
 					</li>,
-					<li key="2">
+					<li key="2" style={{ margin: '0 10px' }}>
+						Credits: {this.props.auth.credits}
+					</li>,
+					<li key="3">
 						<a href="/api/logout">Logout</a>
 					</li>
 				];
@@ -32,7 +35,7 @@ class Header extends Component {
 				<div className="nav-wrapper">
 					<Link
 						to={this.props.auth ? '/surveys' : '/'}
-						className="left-brand-logo"
+						className="left brand-logo"
 					>
 						Emaily
 					</Link>

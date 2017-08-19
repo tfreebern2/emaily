c058babc4607f93828fb4f8a990b354d9f34b40b
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-19 13:57:33 -0400

    Added billing route on backend

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -11,16 +11,17 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

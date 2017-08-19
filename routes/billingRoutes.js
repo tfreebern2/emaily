9ef83efc8986c117645d8e07bbdae41add9ad86c
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-19 14:39:00 -0400

    Finalized Billing Post Path.

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,5 +2,12 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-	app.post('/api/stripe', (req, res) => {});
+	app.post('/api/stripe', async (req, res) => {
+		const charge = await stripe.charges.create({
+			amount: 500,
+			currency: 'usd',
+			description: 'Emaily mailing campaign',
+			source: req.body.id
+		});
+	});
 };

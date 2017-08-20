693ec1f4123274fb7cae1c259c8af660d7448d0e
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-20 10:56:28 -0400

    Added SendGrid Keys and insalled Helper package

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,4 @@
-const mongooes = require('mongoose');
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 

491c99c168cccb861e68b8ee571621374c751526
Timothy Freebern II <tfreebern2@gmail.com>
2017-09-26 13:45:50 -0400

    When SurveyNew form is unmounted Survey Fields are correctly cleared.

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,6 @@
 // Survery New shows SurveryForm and SurveyFormReview
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -8,22 +9,24 @@ class SurveyNew extends Component {
 
 	renderContent() {
 		if (this.state.showFormReview) {
-			return <SurveyFormReview
-				onCancel={() => this.setState({ showFormReview: false })}
-			/>;
+			return (
+				<SurveyFormReview
+					onCancel={() => this.setState({ showFormReview: false })}
+				/>
+			);
 		}
-		return <SurveyForm
-			onSurveySubmit={() => this.setState({ showFormReview: true })}
-		/>;
+		return (
+			<SurveyForm
+				onSurveySubmit={() => this.setState({ showFormReview: true })}
+			/>
+		);
 	}
 
 	render() {
-		return (
-			<div>
-				{this.renderContent()}
-			</div>
-		);
+		return <div>{this.renderContent()}</div>;
 	}
 }
 
-export default SurveyNew;
+export default reduxForm({
+	form: 'surveyForm'
+})(SurveyNew);

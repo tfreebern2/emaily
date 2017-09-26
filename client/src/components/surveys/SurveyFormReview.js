a6835a01a7e4b95bbe18337a4175c8ade8bb5088
Timothy Freebern II <tfreebern2@gmail.com>
2017-09-26 12:08:03 -0400

    Added Back button on SurveyFormReview button. Values from the SurveyForm or not persisting.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const SurveyReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
   return (
     <div>
       <h5>Please confirm your entires</h5>
-      </div>
+      <button
+        className="yellow darken-3 btn-flat"
+        onClick={onCancel}
+      >
+        Back
+      </button>
+    </div>
   );
 };
 
-export default SurveyReview;
+export default SurveyFormReview;
